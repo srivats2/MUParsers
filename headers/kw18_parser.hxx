a00c3ce8d53186ae8696b5a00d9d4b22b38cdefd
Rengarajan Pelapur <rvpnc4@missouri.edu>
2015-07-13 13:45:08 -0500

    All changes made for integrating and combining both parsers in a single package

--- headers/kw18_parser.hxx
+++ headers/kw18_parser.hxx
@@ -1,5 +1,19 @@
 #ifndef __KW18_PARSER_INCLUDED__
 #define __KW18_PARSER_INCLUDED__ 1
+#include<iostream>
+#include<vector>
+#include "RPcustomLOFTconfigparser.hxx"
+#include<ctime>
+#include<string>
+#include<cstring>
 
 
+extern std::vector<int> my_kw18_data_x;
+extern std::vector<int> my_kw18_data_y;
+extern std::vector<int> my_kw18_data_frno;
+extern std::vector<int> my_kw18_data_oid;
+
+extern "C" int kw18_parse(char*);
+
+extern "C" void KW18_parser_help(void);
 #endif
